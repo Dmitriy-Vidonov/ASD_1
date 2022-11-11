f654bb6d690723ee50736b3a842f75f675bf5a0d
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-11-11 18:18:44 +0300

    udpate TestingList. Add clear method for show null list

--- LinkedList_1/src/test/java/mytests/TestIngList.java
+++ LinkedList_1/src/test/java/mytests/TestIngList.java
@@ -2,34 +2,49 @@ package mytests;
 
 import java.util.ArrayList;
 
-public class TestIngList {
+public class TestingList {
     public static void main(String[] args) {
-        Node n1 = new Node(1); // инициализировали первый узел
-        Node n2 = new Node(2); // инициализировали второй узел
-        n1.next = n2; // в указатель первого узла занесли второй узел
+        LinkedList empty_list = new LinkedList();
+
+        Node n1 = new Node(1);
+        Node n2 = new Node(1);
         Node n3 = new Node(1);
-        Node n4 = new Node(3);
-        Node n5 = new Node(1);
-        Node n6 = new Node(1);
+        Node n4 = new Node(1);
+        Node n5 = new Node(2);
+        Node n6 = new Node(3);
         Node n7 = new Node(12);
+
         Node _nodeToInsert = new Node(15);
 
         LinkedList s_list = new LinkedList(); // инициализировали новый список
-        s_list.addInTail(n1); // в конец списка добавили первый узел, указанный ранее
-        s_list.addInTail(n2); // в конец списка добавили второй узел, указанный ранее
+
+        s_list.addInTail(n1);
+    /*    s_list.addInTail(n2);
         s_list.addInTail(n3);
         s_list.addInTail(n4);
         s_list.addInTail(n5);
-       // s_list.addInTail(n6);
-      //  s_list.addInTail(n7);
+        s_list.addInTail(n6);*/
+        //  s_list.addInTail(n7);
 
         s_list.showAll();
-        //System.out.println("remove status = " + s_list.remove(1));
-        s_list.removeAll(1);
+       // System.out.println("remove status = " + s_list.remove(1));
+        // s_list.removeAll(1);
+        s_list.clear();
+        //s_list.remove(1);
         s_list.showAll();
+        System.out.println("head after remove = " + s_list.head);
+        System.out.println("tail after remove = " + s_list.tail);
+        System.out.println("list after remove = " + s_list);
 
-        System.out.println("head = " + s_list.head.value);
-        System.out.println("tail = " + s_list.tail.value);
+
+        System.out.println("пустой список - ");
+        empty_list.showAll();
+        System.out.println("empty head = " + empty_list.head);
+        System.out.println("empty tail = " + empty_list.tail);
+        System.out.println("пустой список вывод - " + empty_list);
+
+       // System.out.println("head = " + s_list.head.value);
+       // System.out.println("tail = " + s_list.tail.value);
 
     }
 }
