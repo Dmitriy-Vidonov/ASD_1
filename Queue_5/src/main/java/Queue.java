6c98c19dc9afce10d7cd34463fb7e0e9d906fce5
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-11-22 23:25:01 +0300

    add Queue class for server check

---
+++ Queue_5/src/main/java/Queue.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Queue<T>
+{
+    public LinkedList<T> QueueList;
+
+    public Queue()
+    {
+        QueueList = new LinkedList<T>();
+    }
+
+    public void enqueue(T item)
+    {
+        QueueList.add(item);
+    }
+
+    public T dequeue()
+    {
+        if(QueueList.size() == 0)
+            return null;
+
+        T element = QueueList.getFirst();
+
+        QueueList.removeFirst();
+        return element;
+    }
+
+    public int size()
+    {
+        return QueueList.size();
+    }
+}
