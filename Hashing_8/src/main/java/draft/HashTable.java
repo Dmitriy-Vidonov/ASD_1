70bced0556b6e48877420e3c24e5899539301e3d
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-12-02 00:15:30 +0300

    add draft methods for HashTable

--- Hashing_8/src/main/java/draft/HashTable.java
+++ Hashing_8/src/main/java/draft/HashTable.java
@@ -6,6 +6,8 @@ public class HashTable
     public int step; // Длина шага, кол-во слотов для поиска след. свободного слота. Например, 3.
     public String [] slots; // Массив для хранения элементов таблицы. Можно сразу сделать его фиксированным.
 
+    protected int loadCount = 0;
+
     public HashTable(int sz, int stp) // Конструктор нашего класса
     {
         size = sz; // Передаем в создаваемый объект размер таблицы
@@ -16,18 +18,54 @@ public class HashTable
 
     public int hashFun(String value)
     {
-        // всегда возвращает корректный индекс слота
-        return 0;
+        int hash;
+        hash = Math.abs(value.hashCode());
+        String hashStr = Integer.toString(hash);
+        hash = Math.abs(hashStr.hashCode());
+
+        hash %= slots.length;
+
+        return hash;
     }
 
     public int seekSlot(String value)
     {
         // находит индекс пустого слота для значения, или -1
-        return -1;
+        int len = slots.length;
+
+        // находит индекс пустого слота для значения, или -1
+        int index = hashFun(value);
+
+        if(loadCount == len)
+            return -1;
+
+        if(slots[index] == null)
+            return index;
+
+        int count = index;
+
+        while (slots[count] != null && loadCount < len) // пока в списке есть свободные места, load_count < len
+        {
+            if((count + step) > (len-1))
+            {
+                count = ((count + step) - len);
+                continue;
+            }
+            count+=1;
+        }
+        return count;
     }
 
     public int put(String value)
     {
+        int index = seekSlot(value);
+
+        if(index != -1)
+        {
+            slots[index] = value;
+            loadCount += 1;
+            return index;
+        }
         // записываем значение по хэш-функции
 
         // возвращается индекс слота или -1
@@ -37,7 +75,29 @@ public class HashTable
 
     public int find(String value)
     {
+        int len = slots.length;
+        int index = hashFun(value);
+
+        if(slots[index].equals(value))
+            return index;
+
+        int counter = 0;
+
+        while ((!slots[index].equals(value)) && (counter < len)) // пока в списке есть свободные места, load_count < len
+        {
+            if((index + step) > (len-1))
+            {
+                index = ((index + step) - len);
+                continue;
+            }
+            index++;
+            counter++;
+        }
+
+        if(!slots[index].equals(value))
+            return -1;
+
+        return index;
         // находит индекс слота со значением, или -1
-        return -1;
     }
 }
--- Hashing_8/src/main/java/draft/hash_fun.java
+++ Hashing_8/src/main/java/draft/hash_fun.java
@@ -33,17 +33,94 @@ public class hash_fun {
         return hash;
     }
 
+    public static int hashFun3(String value, int len)
+    {
+        int hash;
+        hash = Math.abs(value.hashCode());
+        String hashStr = Integer.toString(hash);
+        hash = Math.abs(hashStr.hashCode());
+
+        hash %= len;
+
+        return hash;
+    }
+
+    public static int seekSlot(String value, String[] str, int load_count)
+    {
+        int len = str.length;
+
+        // находит индекс пустого слота для значения, или -1
+        int index = hashFun3(value, len);
+
+        int res = 0;
+
+        if(load_count == len)
+            return -1;
+
+        if(str[index] == null)
+            return index;
+
+        int count = index;
+        int step = 3;
+
+        while (str[count] != null && load_count < len) // пока в списке есть свободные места, load_count < len
+        {
+            if((count + step) > (len-1))
+            {
+                count = ((count + step) - len);
+                continue;
+            }
+
+            count+=1;
+        }
+        return count;
+
+       // return -1;
+    }
+
+    public static int find(String value, String[] str)
+    {
+        int len = str.length;
+        int index = hashFun3(value, len);
+
+        if(str[index].equals(value))
+            return index;
+
+        int step = 3;
+        int counter = 0;
+
+        while ((!str[index].equals(value)) && (counter < len)) // пока в списке есть свободные места, load_count < len
+        {
+            if((index + step) > (len-1))
+            {
+                index = ((index + step) - len);
+                continue;
+            }
+            index++;
+            counter++;
+        }
+
+        if(!str[index].equals(value))
+            return -1;
+
+        return index;
+        // находит индекс слота со значением, или -1
+    }
+
     public static void main(String[] args) {
 
         System.out.println("hashCode ba = " + hashFun2("ba", 17));
         System.out.println("hashCode ab = " + hashFun2("ab", 17));
 
-        String[] strArr = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen"};
+        String[] strArr = {"one", "three", "four", "five", "six", "seven", "ten10", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen"};
 
         for(int i=0; i<strArr.length; i++)
         {
-            System.out.println(hashFun(strArr[i], strArr.length));
+          //  System.out.println(hashFun3(strArr[i], strArr.length));
         }
+
+        System.out.println("null индекс - " + seekSlot("one", strArr, 17));
+        System.out.println("find индекс - " + find("four", strArr));
     }
 }
