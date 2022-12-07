341437911b4d5ce0db80e75a9d6a12c176cbca6b
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-12-07 22:25:09 +0300

    add subset test for PowerSet

--- Sets_10/.idea/sonarlint/issuestore/index.pb
+++ Sets_10/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,13 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+Q
+!src/main/java/draft/PowerSet.java,3/d/3df896cd4e7bb92939da2dcb9acd7aa360027bcf
+U
+%src/main/java/draft/TestPowerSet.java,9/7/97af6016095d86fafc9d188732bcfcab3e2b5b5f
+P
+ src/test/java/PutRemoveTest.java,a/9/a90803bfa5aa67fc83073c0e708be95bec1d9573
+L
+src/test/java/UnionTest.java,c/a/ca25aa66e69ccb3ec3f8d96f51f82d94599286f6
+S
+#src/test/java/IntersectionTest.java,1/4/14fea810a67ae4250ccd2a4cdd550859a29f8620
---
+++ Sets_10/src/test/java/SubsetTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubsetTest {
+
+    PowerSet pwrSet1 = new PowerSet();
+    PowerSet pwrSet2 = new PowerSet();
+    PowerSet pwrSet3 = new PowerSet();
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        pwrSet1.put("10");
+        pwrSet1.put("20");
+        pwrSet1.put("40");
+        pwrSet1.put("100");
+
+        pwrSet2.put("20");
+        pwrSet2.put("40");
+
+        pwrSet3.put("20");
+        pwrSet3.put("40");
+        pwrSet3.put("50");
+    }
+
+    @Test
+    @DisplayName("1) Все элементы параметра входят в текущее множество")
+    void isSubset1() throws Exception
+    {
+        assertTrue(pwrSet1.isSubset(pwrSet2));
+    }
+
+    @Test
+    @DisplayName("2) Все элементы текущего множества входят в параметр")
+    void isSubset2() throws Exception
+    {
+        assertFalse(pwrSet2.isSubset(pwrSet1));
+    }
+
+    @Test
+    @DisplayName("3) Не все элементы параметра входят в текущее множество")
+    void isSubset3() throws Exception
+    {
+        assertFalse(pwrSet1.isSubset(pwrSet3));
+    }
+}
