d131fe0cab418a9010f30195b28d7bd013e9f0b5
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-12-10 03:00:00 +0300

    add draft2 methods for PowerSet using handmade HashTable

--- Sets_10/src/main/java/draft2/PowerSet.java
+++ Sets_10/src/main/java/draft2/PowerSet.java
@@ -1,29 +1,40 @@
 package draft2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PowerSet
 {
     public int capacity;
     public int step;
     public String [] slots;
-    protected int loadCount = 0;
+    protected java.util.List<Integer> indexes; // Массив для хранения индексов элементов
 
     public PowerSet()
     {
-        capacity = 20000;
-        step = 3;
+        capacity = 10;
+        step = 1;
         slots = new String[capacity];
-        for(int i=0; i<capacity; i++) slots[i] = null;
+        indexes = new ArrayList<Integer>();
+        for(String slot : slots) slot = null;
         // ваша реализация хранилища
     }
 
     public int size()
     {
-        return loadCount;
+        return indexes.size();
     }
-
 
     public void put(String value)
     {
+        int index = seekSlot(value);
+
+        if(index != -1 && this.get(value) == false)
+        {
+            slots[index] = value;
+            indexes.add(index);
+        }
+        //return;
         // всегда срабатывает
     }
 
@@ -31,11 +42,38 @@ public class PowerSet
     {
         // возвращает true если value имеется в множестве,
         // иначе false
-        return false;
+        boolean res = false;
+
+        int len = slots.length;
+        int index = hashFun(value);
+
+        if(slots[index] == value)
+            return true;
+
+        for(int i=0; i<len; i++)
+        {
+            index += step;
+
+            if(index > len-1)
+                index-=len;
+
+            if(slots[index] == value)
+            {
+                return true;
+            }
+        }
+        return res;
     }
 
     public boolean remove(String value)
     {
+        int findIndex = find(value);
+
+        if(findIndex != -1)
+        {
+            slots[findIndex] = null;
+            return true;
+        }
         // возвращает true если value удалено
         // иначе false
         return false;
@@ -43,28 +81,82 @@ public class PowerSet
 
     public PowerSet intersection(PowerSet set2)
     {
+        PowerSet pwrSet = new PowerSet();
+
+        for(int i=0; i<indexes.size(); i++) { // Проходим по массиву с индексами
+            if(set2.get(this.slots[indexes.get(i)]))  // Если элемент по индексу из основного множества есть во втором, то:
+                pwrSet.put(this.slots[indexes.get(i)]); // Кладем его в pwrSet
+        }
+
+        return pwrSet;
         // пересечение текущего множества и set2
-        return null;
     }
 
     public PowerSet union(PowerSet set2)
     {
+        PowerSet pwrSet = new PowerSet();
+
+        int mainSize = this.size();
+        int secondSize = set2.size();
+
+        if(mainSize == 0 && secondSize == 0)
+        {
+            return pwrSet;
+        }
+
+        if(mainSize == 0 && secondSize != 0)
+        {
+            return set2;
+        }
+
+        if(mainSize >= secondSize)
+        {
+            this.clone(pwrSet);
+
+            for(int i=0; i<set2.indexes.size(); i++) {
+                if(!this.get(set2.slots[set2.indexes.get(i)]))
+                    pwrSet.put(set2.slots[set2.indexes.get(i)]);
+            }
+
+            return pwrSet;
+        }
+
+        for(int i=0; i<this.indexes.size(); i++) {
+            if(!set2.get(this.slots[this.indexes.get(i)]))
+                pwrSet.put(this.slots[this.indexes.get(i)]);
+        }
+
+        return pwrSet;
         // объединение текущего множества и set2
-        return null;
     }
 
     public PowerSet difference(PowerSet set2)
     {
+        PowerSet pwrSet = new PowerSet();
+
+        for(int i=0; i<this.indexes.size(); i++) {
+            if(!set2.get(this.slots[this.indexes.get(i)]))
+                pwrSet.put(this.slots[this.indexes.get(i)]);
+        }
+
+        return pwrSet;
         // разница текущего множества и set2
-        return null;
     }
 
     public boolean isSubset(PowerSet set2)
     {
+        int counter = 0;
+
+        for(int i=0; i<set2.indexes.size(); i++) {
+            if(this.get(set2.slots[set2.indexes.get(i)]))
+                counter++;
+        }
+
+        return (set2.size() == counter);
+
         // возвращает true, если set2 есть
         // подмножество текущего множества,
         // иначе false
-        return false;
     }
 
     public int hashFun(String value)
@@ -81,6 +173,8 @@ public class PowerSet
 
     public int seekSlot(String value)
     {
+        int loadCount = indexes.size();
+
         int len = slots.length;
         int index = hashFun(value);
 
@@ -111,7 +205,7 @@ public class PowerSet
         return res;
     }
 
-    public int add(String value)
+   /* public int add(String value)
     {
         int index = seekSlot(value);
         if(index != -1 && value != slots[index])
@@ -121,13 +215,16 @@ public class PowerSet
             return index;
         }
         return -1;
-    }
+    }*/
 
     public int find(String value)
     {
         int res = -1;
         int len = slots.length;
         int index = hashFun(value);
+
+        if(slots[index] == value)
+            return index;
 
         for(int i=0; i<len; i++)
         {
@@ -145,10 +242,21 @@ public class PowerSet
         return res;
     }
 
+    public void clone(PowerSet set2)
+    {
+        for(int i=0; i<this.indexes.size(); i++)
+            set2.put(this.slots[this.indexes.get(i)]);
+    }
+
     public void ShowTable()
     {
         for (int i=0; i<slots.length; i++)
             System.out.print(slots[i] + " ");
+        System.out.println();
+        System.out.println("indexes:");
+
+        for (int i=0; i<indexes.size(); i++)
+            System.out.print(indexes.get(i) + " ");
         System.out.println();
     }
 
--- Sets_10/src/main/java/draft2/testSet.java
+++ Sets_10/src/main/java/draft2/testSet.java
@@ -4,12 +4,35 @@ public class testSet {
     public static void main(String[] args)
     {
         PowerSet set1 = new PowerSet();
+        PowerSet set2 = new PowerSet();
+        PowerSet emptySet1 = new PowerSet();
+        PowerSet emptySet2 = new PowerSet();
+        PowerSet cloneSet = new PowerSet();
 
-        set1.add("one");
-        set1.add("two");
-        set1.add("three");
-        set1.add("one");
+        set1.put("10");
+        set1.put("20");
+        set1.put("40");
+        set1.put("100");
 
-        set1.ShowTable();
+        //set2.put("50");
+        //set2.put("80");
+        //set2.put("100");
+        set2.put("20");
+        set2.put("40");
+
+        set1.clone(cloneSet);
+
+        //set1.ShowTable();
+        //set2.ShowTable();
+        cloneSet.ShowTable();
+
+        System.out.println("size of clone: " + cloneSet.size());
+
+       /* System.out.print("Intersection: "); set1.intersection(set2).ShowTable();
+        System.out.print("Union: "); set1.union(set2).ShowTable();
+        System.out.print("Union with empty: "); set1.union(emptySet1).ShowTable();*/
+        //System.out.print("Union two emptys: "); emptySet1.union(emptySet2).ShowTable();
+        //System.out.print("Difference: "); set1.difference(set2).ShowTable();
+        System.out.print("Subset: " + set1.isSubset(set2));
     }
 }
