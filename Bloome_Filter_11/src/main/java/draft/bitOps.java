fb2dc90fcb793b3eb2acd9f7b09a4da9e71c3fbf
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-12-15 22:35:54 +0300

    drafts methods for hashing Bloom filter and also operations with bits examples

--- Bloome_Filter_11/src/main/java/draft/bitOps.java
+++ Bloome_Filter_11/src/main/java/draft/bitOps.java
@@ -2,6 +2,7 @@ package draft;
 
 public class bitOps {
 
+    // Установить бит в единицу по нужному индексу
     public static int setBit(int value, int bitIndex)
     {
         int res = value;
@@ -9,6 +10,7 @@ public class bitOps {
         return res;
     }
 
+    // Установить бит в ноль по нужному индексу
     public static int removeBit(int value, int bitIndex)
     {
         int res = value;
@@ -16,17 +18,25 @@ public class bitOps {
         return res;
     }
 
+    // Получить значение бита по указанному индексу
     public static int getBit(int value, int bitIndex)
     {
         int res = value & (1<<bitIndex);
         return (res > 0) ? 1 : 0;
     }
 
-    public static int firstHashFunc()
+    // Первая хэш-функция с рандомным числом = 17
+    public int hash1(String str1)
     {
-        int res = 0;
-
-        return res;
+        int result = 0;
+        // 17
+        for(int i=0; i<str1.length(); i++) // цикл до длины строки, рез-тат с предыдущ. итерации на 17 + код символа
+        {
+            int code = (int)str1.charAt(i);
+            // в качестве рез-тата первой итерации использовать ноль
+        }
+        // реализация ...
+        return 0;
     }
 
     public static int secondHashFunc()
@@ -50,6 +60,15 @@ public class bitOps {
         // массив m - 32 разрядное число. Это обычный int
 
         // как перевести int в бинарную систему?
+        int m1 = 0;
+        m1 = setBit(m1, 0); // m1 = 1
+        m1 = setBit(m1, 1); // m1 = 3
+        m1 = setBit(m1, 2); // m1 = 7
+        m1 = removeBit(m1, 1); // m1 = 5
+
+        System.out.println("m1 = " + toBinaryString(m1));
+        System.out.println("m1 = " + m1);
+
         int m = 42;
         System.out.println(toBinaryString(m));
 
@@ -61,5 +80,37 @@ public class bitOps {
 
         m = removeBit(m, 1);
         System.out.println(toBinaryString(m));
+
+        // Тестирование цикла для хэш-функции
+        int res = 0;
+        String str1 = "three";
+        int rand = 17;
+
+        for(int i=0; i<str1.length(); i++)
+            System.out.println(String.valueOf(str1.charAt(i)) + " = " + (int)str1.charAt(i));
+
+
+        // 1-я хэш-функция
+        for(int i=0; i<str1.length(); i++)
+        {
+            if (i == 0) continue;
+            res += (res * rand) + (int)str1.charAt(i);
+        }
+        int index = res % str1.length();
+
+        System.out.println("index = " + index); // index = 3 корректный результат
+
+        // 2-я хэш-функция
+        res = 0;
+        rand = 223;
+        for(int i=0; i<str1.length(); i++)
+        {
+            if (i == 0) continue;
+            res += (res * rand) + (int)str1.charAt(i);
+        }
+        int index2 = res % str1.length();
+
+        System.out.println("index2 = " + index2); // index2 = 0 корректный результат
+
     }
 }
