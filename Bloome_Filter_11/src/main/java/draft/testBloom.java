ea62c88bbbd5cb9fe9658408de9b6dad87484fac
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-12-16 00:00:38 +0300

    draft version of BloomFilter with full explanation
    With plenty of comments all features and logic of how Bloom filter works are explained.

--- Bloome_Filter_11/src/main/java/draft/testBloom.java
+++ Bloome_Filter_11/src/main/java/draft/testBloom.java
@@ -3,6 +3,10 @@ package draft;
 public class testBloom {
     public static void main(String[] args)
     {
-        System.out.println("Say hi");
+        BloomFilter bloom1 = new BloomFilter(32);
+        bloom1.add("three");
+
+        System.out.println(bitOps.toBinaryString(bloom1.bitStorage));
+        System.out.println(bloom1.isValue("three"));
     }
 }
